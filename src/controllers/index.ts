62b09c172713bd829a48ab9cb4ac8cb1a4976d32
Julia Shestakova <greenroach@gmail.com>
2018-10-21 19:14:28 +0300

    error handling

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,6 +4,6 @@ import { Request, Response } from "express";
  * GET /
  * Home page.
  */
-export let index = (req: Request, res: Response) => {
-	res.render("index", { title: "Express" });
+export let index = async (req: Request, res: Response) => {
+    res.render("index", { title: "Express" });
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
 import { app } from "./app";
 
-const server = app.listen(app.get("port"), () => {
-    console.log(("Server running at http://localhost:%d in %s mode"),
-    app.get("port"), app.get("env"));
-    console.log("Press CTRL-C to stop\n");
-});
+const port = app.get("port");
+
+const server = app.listen(port, onListening);
+server.on("error", onError);
+
+function onError(error: any) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    const bind = typeof port === "string"
+        ? "Pipe " + port
+        : "Port " + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === "string"
+      ? "pipe " + addr
+      : "port " + addr.port;
+    console.log("Listening on " + bind);
+}
 
 export default server;
