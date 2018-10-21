e110ecd21e36f62c4f228537058220eeed7e1927
Julia Shestakova <greenroach@gmail.com>
2018-10-21 14:21:44 +0300

    update indents

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app";
 
 const server = app.listen(app.get("port"), () => {
-	console.log(("Server running at http://localhost:%d in %s mode"),
-	app.get("port"), app.get("env"));
-	console.log("Press CTRL-C to stop\n");
+    console.log(("Server running at http://localhost:%d in %s mode"),
+    app.get("port"), app.get("env"));
+    console.log("Press CTRL-C to stop\n");
 });
 
 export default server;
