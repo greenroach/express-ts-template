6ece15966fb25da102bdf34c4980857b66cbcae1
Julia Shestakova <greenroach@gmail.com>
2021-03-11 21:21:07 +0300

    Add esModuleInterop

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import * as express from "express";
-import * as logger from "morgan";
+import express from "express";
+import logger from "morgan";
 import * as path from "path";
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import * as createError from "http-errors";
+import createError from "http-errors";
 
 declare type WebError = Error & { status?: number };
 export const errorHandler = (err: WebError, req: Request, res: Response, next: NextFunction): void => {
@@ -7,7 +7,6 @@ export const errorHandler = (err: WebError, req: Request, res: Response, next: N
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    console.log(123);
     // render the error page
     res.status(err.status || 500);
     res.render("error", { title: err.name, message: err.message });
